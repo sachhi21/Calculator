2c7b6c4dc5da7fa6c146792034023c395c7f7f92
Sachin Rampur <99388531+sachhi21@users.noreply.github.com>
2023-04-27 15:11:04 +0530

    Add files via upload

---
+++ Calculator_app/Calculator_app/Operations.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_app
+{
+    class Calculator
+    {
+        /*public void Addition()
+        {
+            int result = 0;
+            Console.WriteLine("Enter no. of Numbers to be added");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the numbers you want to add:");
+            for (int i = 0; i < a; i++)
+            {
+                Console.WriteLine("Number {0}", i + 1);
+                int b = Convert.ToInt32(Console.ReadLine());
+                result = result + b;
+            }
+            Console.WriteLine("Sum: {0}", result);
+        }
+        public void Subtraction()
+        {
+            int result = 0;
+            Console.WriteLine("Enter numbers to be subtracted:");
+            Console.WriteLine("Number 1:");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Number 2:");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+
+            result = input1 - input2;
+
+            Console.WriteLine("After Subtraction: {0}", result);
+        }*/
+        public void Addition()
+        {
+            List<int> subList = new List<int>();
+            Console.WriteLine("Enter no. of Numbers to be added");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the numbers you want to add:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Number {0}", i + 1);
+                subList.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+            Console.WriteLine("Sum: {0}", subList.Sum());
+        }
+        public void Subtraction()
+        {
+            List<int> subList = new List<int>();
+            Console.WriteLine("Enter no. of Numbers to be subtracted");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int result = 0;
+            Console.WriteLine("Enter the numbers you want to subtract:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Number {0}", i + 1);
+                subList.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+            foreach (var item in subList)
+            {
+                if (result == 0)
+                {
+                    result = item;
+                }
+                else
+                {
+                    result -= item;
+                }
+            }
+            Console.WriteLine("After Subtraction: {0}", result);
+        }
+        public void Multiplication()
+        {
+            int result = 1;
+            Console.WriteLine("Enter no. of Numbers to be multiplied");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the numbers you want to multiply:");
+            for (int i = 0; i < a; i++)
+            {
+                Console.WriteLine("Number {0}", i + 1);
+                int b = Convert.ToInt32(Console.ReadLine());
+                result = result * b;
+            }
+            Console.WriteLine("After multiplication: {0}", result);
+        }
+        public void Division()
+        {
+            float result;
+            Console.WriteLine("Enter numbers to be divided:");
+            Console.WriteLine("Number 1:");
+            double input1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Number 2:");
+            double input2 = Convert.ToDouble(Console.ReadLine());
+
+            result = (float)(input1 / input2);
+
+            Console.WriteLine("After Division: {0}", result);
+        }
+        public void squroot()
+        {
+            Console.WriteLine("Enter a number to find it's Square-Root");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            if (num1 < 0)
+            {
+                Console.WriteLine("Cannot take square root of a negative number!");
+            }
+            else
+            {
+                Console.WriteLine($"Result: {Math.Sqrt(num1)}");
+            }
+        }
+        public void exponent()
+        {
+            Console.WriteLine("Enter Base number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Power:");
+            double exp = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"{num1}^{exp}: {Math.Pow(num1, exp)}");
+        }
+    }
+}
---
+++ Calculator_app/Calculator_app/Program.cs
@@ -0,0 +1,187 @@
+﻿/*using Calculator_app;
+using System;
+using System.Runtime.CompilerServices;
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        int action;
+        bool flag = true;
+        void Menu()
+        {
+            Console.WriteLine("Enter the action to be performed");
+            Console.WriteLine("Press 1 for Addition");
+            Console.WriteLine("Press 2 for Subtraction");
+            Console.WriteLine("Press 3 for Multiplication");
+            Console.WriteLine("Press 4 for Division");
+            Console.WriteLine("Press 5 for exit");
+            action = Convert.ToInt32(Console.ReadLine());
+        }
+        void Repeat()
+        {
+            Console.WriteLine("Do you want to continue?");
+            string a = Console.ReadLine();
+            if (a == "y" || a == "yes")
+            {
+                Menu();
+            }
+            else
+            {
+                flag = false;
+            }
+        }
+        Menu();
+
+        Calculator cal = new Calculator();
+
+        while (flag)
+        {
+            switch (action)
+            {
+                case 1:
+                    cal.Addition();
+                    Repeat();
+                    break;
+                case 2:
+                    cal.Subtraction();
+                    Repeat();
+                    break;
+                case 3:
+                    cal.Multiplication();
+                    Repeat();
+                    break;
+                case 4:
+                    cal.Division();
+                    Repeat();
+                    break;
+                case 5:
+                    flag = false;
+                    break;
+            }
+        }
+    }
+}*/
+
+using Calculator_app;
+using System.Data;
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        int action;
+        int choose;
+        Main_menu();
+        void Main_menu()
+        {
+            Console.WriteLine("-----CALCULATOR MAIN MENU-----");
+            Console.WriteLine("1: Basic Operations  2: Mixed Operations");
+            choose = Convert.ToInt32(Console.ReadLine());
+        }
+
+        bool flag = true;
+
+        void Mixed_OP()
+        {
+            string input;
+            double result;
+            DataTable dt = new DataTable();
+            Console.WriteLine("Calculator Program");
+            Console.WriteLine("------------------");
+            Console.WriteLine("Enter your calculations (e.g. 1+8*9/8+5), then press Enter:");
+            Console.WriteLine("Type 'exit' to quit");
+            while (true)
+            {
+                Console.WriteLine("Enter the Expression for evaluation");
+                input = Console.ReadLine();
+                if (input.ToLower() == "exit")
+                {
+                    break;
+                }
+                try
+                {
+                    result = Convert.ToDouble(dt.Compute(input, ""));
+                    Console.WriteLine("Result: " + result);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+        }
+
+        if (choose == 1)
+        {
+            Console.WriteLine("---You are in BASIC OPERATIONS Section---");
+
+            void Menu()
+            {
+                Console.WriteLine("Enter the action to be performed");
+                Console.WriteLine("Press 1 for Addition and hit Enter");
+                Console.WriteLine("Press 2 for Subtraction and hit Enter");
+                Console.WriteLine("Press 3 for Multiplication and hit Enter");
+                Console.WriteLine("Press 4 for Division and hit Enter");
+                Console.WriteLine("Press 5 for Square-Root and hit Enter");
+                Console.WriteLine("Press 6 for Exponent and hit Enter");
+                Console.WriteLine("Press 7 to EXIT and hit Enter");
+                action = Convert.ToInt32(Console.ReadLine());
+            }
+            void Repeat()
+            {
+                Console.WriteLine("Do you wish to continue? (y/n)");
+                string a = Console.ReadLine();
+                if (a == "y" || a == "yes")
+                {
+                    Menu();
+                }
+                else
+                {
+                    flag = false;
+                }
+            }
+            Menu();
+            Calculator cal = new Calculator();
+            while (flag)
+            {
+                switch (action)
+                {
+                    case 1:
+                        cal.Addition();
+                        Repeat();
+                        break;
+                    case 2:
+                        cal.Subtraction();
+                        Repeat();
+                        break;
+                    case 3:
+                        cal.Multiplication();
+                        Repeat();
+                        break;
+                    case 4:
+                        cal.Division();
+                        Repeat();
+                        break;
+                    case 5:
+                        cal.squroot();
+                        Repeat();
+                        break;
+                    case 6:
+                        cal.exponent();
+                        Repeat();
+                        break;
+                    case 7:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Option");
+                        flag = false;
+                        break;
+                }
+            }
+        }
+        else if (choose == 2)
+        {
+            Mixed_OP();
+        }
+    }
+}
